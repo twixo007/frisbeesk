ef986ab3b2ce8f89459d2b278ddb81de715a6464
Matej <matej.randik@gmail.com>
2016-12-17 13:19:05 +0100

    code

---
+++ Frisbee/frisbee/apps.py
@@ -0,0 +1,6 @@
+#-*- coding: utf-8 -*-
+from django.apps import AppConfig
+from django.utils.encoding import smart_unicode
+class YourAppConfig(AppConfig):
+    name = 'frisbee'
+verbose_name = smart_unicode('Frisbee')
